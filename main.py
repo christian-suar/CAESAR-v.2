6ad34029353f800d303b8ba61e9fe1a99e449231
christian-suar <148103464+christian-suar@users.noreply.github.com>
2024-09-30 11:12:19 -0500

    works

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
-message = "abcd efASDASDASDj hijk lmnop qrs tuv wx yz 0 1 2 345 6789 "
-number = 5
 
-def ceaser_cipher(text:str, shift: int):
+message = "the 1 Quick Fox Jumped over the rabbit or something 432 times."
+character_list = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+num = 101
+
+
+def ceaser_cipher(text:str, shift:str):
     result = ""
     for char in text:
-        if char.islower():
-            char = char.upper()
-        elif char.isupper():
-            result += chr((ord(char) + shift - 65) % 26 + 65)
-        elif char.isdigit():
-            result += chr((ord(char) + shift - 48) % 10 + 48)
+        if char in character_list:
+            result += character_list[(character_list.index(char)+shift)%62] # first instance of letter from text in charlist, shift by 101, divide by 62 for remainder for new char from charlist
         else:
             result += char
 
     return result
 
-def ceaser_decipher(secret_message, shift): 
+def ceaser_decipher(secret_message:str, shift:int): 
     result = ""
     for char in secret_message:
-        if char.islower():
-            result += chr((ord(char) - shift - 97) % 26 + 97)
-        elif char.isupper():
-            result += chr((ord(char) - shift - 65) % 26 + 65)
-        elif char.isdigit():
-            result += chr((ord(char) - shift - 48) % 10 + 48)
+        if char in character_list:
+            result += character_list[(character_list.index(char)-shift)%62]
         else:
             result += char
 
@@ -32,11 +27,11 @@ def ceaser_decipher(secret_message, shift):
 
 print("#########################################################################################################")
 print("Encrypted: ")
-print(ceaser_cipher(message, number))
+print(ceaser_cipher(message,num))
 
 print()
 
-secret_message = ceaser_cipher(message, number)
+secret_message = ceaser_cipher(message,num)
 print("Decrypted (original message): ")
-print(ceaser_decipher(secret_message, number))
+print(ceaser_decipher(secret_message,num))
 print("#########################################################################################################")
